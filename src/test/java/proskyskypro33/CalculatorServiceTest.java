307ce5d265b73b8fc917bf253c25a7405496149c
EosReign <vsevolod_sol@mail.ru>
2022-10-17 21:17:43 +0300

    Easy

--- src/test/java/proskyskypro33/CalculatorServiceTest.java
+++ src/test/java/proskyskypro33/CalculatorServiceTest.java
@@ -49,41 +49,4 @@ public class CalculatorServiceTest {
         assertThat(actual).isEqualTo(-3);
     }
 }
-/*
-@GetMapping(path = "/calculator/plus")
-    public String plus(@RequestParam("num1") int num1,
-                       @RequestParam("num2") int num2) {
-        if (num1 != 0 && num2 != 0) {
-            return num1 + " + " + num2 + " = " + calculatorService.plus(num1, num2);
-        } else {
-            return "Error";
-        }
-    }
-    @GetMapping(path = "/calculator/minus")
-    public String minus(@RequestParam("num1") int num1,
-                        @RequestParam("num2") int num2) {
-        if (num1 != 0 && num2 != 0) {
-            return num1 + " - " + num2 + " = " + calculatorService.minus(num1, num2);
-        } else {
-            return "Error";
-        }
-    }
-    @GetMapping(path = "/calculator/multiply")
-    public String multiply(@RequestParam("num1") int num1,
-                           @RequestParam("num2") int num2) {
-        if (num1 != 0 && num2 != 0) {
-            return num1 + " * " + num2 + " = " + calculatorService.multiply(num1, num2);
-        } else {
-            return "Error";
-        }
-    }
-    @GetMapping(path = "/calculator/divide")
-    public String divide(@RequestParam("num1") int num1,
-                         @RequestParam("num2") int num2) {
-        if (num1 != 0 && num2 != 0) {
-            return num1 + " / " + num2 + " = " + calculatorService.divide(num1, num2);
-        } else {
-            return "Error";
-        }
-    }
- */
+
